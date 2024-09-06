14be5ccb23a41154d1096821898a926da2811c1d
Sebastian Castañeda <sedriast.developer@gmail.com>
2024-09-06 17:49:38 -0500

    feat: Actualizar componentes de diseño y agregar formulario modal de elementos

--- components/fragments/Forms/ItemForm.tsx
+++ components/fragments/Forms/ItemForm.tsx
@@ -1,8 +1,15 @@
 import React from "react";
-import { StyleSheet, View } from "react-native";
+import { StyleSheet, Text, TextInput, View } from "react-native";
 
 export default function ItemForm() {
-	return <View style={st.body}></View>;
+	return (
+		<View style={st.body}>
+			<Text>ItemForm</Text>
+			<TextInput style={st.inputs} placeholder="Nombre" />
+			<Text>ItemForm</Text>
+			<TextInput style={st.inputs} placeholder="Fecha final" />
+		</View>
+	);
 }
 
 const st = StyleSheet.create({
@@ -11,5 +18,18 @@ const st = StyleSheet.create({
 		height: 1000,
 		flexDirection: "column",
 		backgroundColor: "#0000",
+	},
+	inputs: {
+		width: "90%",
+		height: 45,
+		fontSize: 20,
+		borderWidth: 1,
+		marginLeft: 10,
+		marginRight: 10,
+		borderStyle: "solid",
+		paddingHorizontal: 20,
+		borderColor: "#0E7490",
+		backgroundColor: "#A5F3FC",
+		marginTop: 10,
 	},
 });
